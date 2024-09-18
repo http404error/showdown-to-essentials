08ce78feaf4b9b975d0451c21d896dfbfe274d52
Benjamin Arvey <404bennotfound@gmail.com>
2024-09-18 12:07:06 -0700

    start to add proper substitutions - EVs and Moves slapdash for now

--- translator.js
+++ translator.js
@@ -1,7 +1,16 @@
 function convert() {
   text = document.getElementById("showdown-text").value
 
-  text = text.replaceAll('hello', 'goodbye')
+  const speciesItemFind = /^(.*) ?@ ?(.*)$/g
+  text = text.replaceAll(speciesItemFind, /Pokemon = $1\n    Item = $2\n/)
+  const abilityFind = /^Ability: ?(.*)$/g
+  text = text.replaceAll(abilityFind, /    Ability = $1\n/)
+  const evsFind = /^EVs: (.*)$/g
+  text = text.replaceAll(evsFind, /    EVs = $1\n/)
+  const natureFind = /^(.*) Nature$/g
+  text = text.replaceAll(natureFind, /    Nature = $1\n/)
+  const moveFind = /^- (.*)[\n- (.*)]?[\n- (.*)]?[\n- (.*)]?/g
+  text = text.replaceAll(moveFind, /    Moves = $1,$2,$3,$4\n/)
 
   document.getElementById("essentials-text").value = text
 }
