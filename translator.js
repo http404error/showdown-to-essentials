f939dccfe530d18f825c2f774f96e39136d71773
Benjamin Arvey <404bennotfound@gmail.com>
2024-09-30 12:04:02 -0700

    IVs default to max now

--- translator.js
+++ translator.js
@@ -31,6 +31,7 @@ function importTeams(buffer) {
       curSet = null;
     } else if (!curSet) {
       curSet = { name: '', species: '', gender: '', level: 100 };
+      curSet.ivs = { hp: 31, atk: 31, def: 31, spa: 31, spd: 31, spe: 31 };
       team.push(curSet);
       var atIndex = line.lastIndexOf(' @ ');
       if (atIndex !== -1) {
